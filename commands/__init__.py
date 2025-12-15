6a2dc5a22ef59ef7f517f3d75ac13b7e56491fce
realcathode <ion.miron@tutanota.com>
2025-12-15 21:08:21 +0200

    Register new commands in the command registry

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,6 +1,7 @@
 from commands.base_command import BaseCommand
 from .scan_command import ScanCommand
-
+from .interface_monitor import MonitorCommand
+from .interface_managed import ManagedCommand
 COMMAND_REGISTRY = {
     cls.NAME: cls
     for cls in BaseCommand.__subclasses__()
